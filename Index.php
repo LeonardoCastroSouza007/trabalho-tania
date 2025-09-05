e97bb5afbfa488d7569b40642502be9eb41ad0a2
LeonardoCastroSouza007 <leonardo.castro.souza@edu.unifil.br>
2025-09-04 21:55:32 -0300

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Métodos Numéricos em PHP</title>
+</head>
+<body>
+    <h1>Métodos Numéricos</h1>
+    <ul>s
+        <li><a href="gauss_seidel.php">Gauss-Seidel</a></li>
+        <li><a href="bisseccao.php">Bissecção</a></li>
+    </ul>
+</body>
+</html>
